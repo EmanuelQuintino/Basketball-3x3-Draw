f900620dd8d4200fc78908e3913f55bace2465da
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-07-13 13:26:35 -0300

    add base html

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppRoutes } from "./routes/index.tsx";
+import { GlobalStyles } from "./styles/global.ts";
+import { ThemeProvider } from "styled-components";
+import { appTheme } from "./styles/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppRoutes />
+    <ThemeProvider theme={appTheme}>
+      <AppRoutes />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,5 +1,85 @@
+import { Link } from "react-router-dom";
 import { Container } from "./style";
+import logoReprograma from "../../assets/logo-reprograma-jucas.png";
 
 export function App() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <header>
+        <img src="../../assets/basquete3x3.png" alt="" />
+      </header>
+
+      <main>
+        <article>
+          <section>
+            <strong className="nameDraw">-</strong>
+          </section>
+
+          <section className="drawButtons">
+            <button className="drawButton">Sortear</button>
+            <button className="clearButton">Limpar</button>
+          </section>
+        </article>
+
+        <section className="drawTable">
+          <table>
+            <thead>
+              <tr>
+                <th>TIME 1</th>
+                <th>TIME 2</th>
+                <th>TIME 3</th>
+                <th>TIME 4</th>
+                <th>TIME 5</th>
+                <th>TIME 6</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr className="pot1">
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+
+              <tr className="pot2">
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+
+              <tr className="pot3">
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+
+              <tr className="pot4">
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </main>
+
+      <footer>
+        <Link to={""}>
+          <img src={logoReprograma} />
+        </Link>
+      </footer>
+    </Container>
+  );
 }
