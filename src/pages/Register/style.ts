fddfc520fec1c0860246065b2f2d3c9ceacf8f34
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-07-17 19:34:22 -0300

    add responsive desktop register

--- src/pages/Register/style.ts
+++ src/pages/Register/style.ts
@@ -1,20 +1,21 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 0 2.4rem;
+  padding: 0 1.2rem;
 
   .pots {
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(2, 1fr);
     gap: 0.8rem;
-    margin-top: 2rem;
+    margin-top: 1.2rem;
   }
 
-  @media (max-width: 768px) {
-    padding: 0 1.2rem;
+  @media (min-width: 768px) {
+    padding: 0 2.4rem;
 
     .pots {
-      grid-template-columns: repeat(2, 1fr);
+      grid-template-columns: repeat(4, 1fr);
+      margin-top: 2rem;
     }
   }
 `;
